257b37f6d0cf456ec701fde14819d810da6b47a5
valerakozhanov <valerykozhanov@gmail.com>
2017-09-11 19:11:17 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view/view.component';
+import { MyHoverDirective } from './directives/my-hover.directive';
+import { MyCouterDirective } from './directives/my-couter.directive';
+import { SpaceRemoveDirective } from './directives/space-remove.directive';
+import { Simple } from './pipes/simple.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    MyHoverDirective,
+    MyCouterDirective,
+    SpaceRemoveDirective,
+    Simple
   ],
   imports: [
     BrowserModule,
